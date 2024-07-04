508eb17336f87f49496e4aa0cac5913e14869148
ozgureroglu01 <157051743+ozgureroglu01@users.noreply.github.com>
2024-07-04 17:54:41 +0300

    Copmpeted the script for rock paper scissors game

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const choices = ["Rock", "Paper", "Scissors"];
+const choices = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
     let randomIndex = Math.floor(Math.random() * choices.length);
@@ -8,9 +8,45 @@ function getComputerChoice() {
 function getHumanChoice() {
     let userChoice;
     do {
-        userChoice = prompt("Your choice: ");
+        userChoice = prompt("Your choice: ").toLowerCase();
     } while (!choices.includes(userChoice));
 
     return userChoice;
 
 }
+
+let humanScore = 0;
+let computerScore = 0;
+let drawScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("It's a draw!");
+        return drawScore++;
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("You win! Rock beats Scissors!");
+        return humanScore++;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("You win! Paper beats rock!");
+        return humanScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("You win! Scissors beats paper!");
+        return humanScore++;
+    } else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}!`);
+        return computerScore++;
+    }
+}
+
+
+
+function playGame(rounds) {
+    for (let i = 0; i < rounds; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+    console.log(`Player score: ${humanScore}, Computer score ${computerScore}, Draw: ${drawScore}`);
+}
+
+playGame(5);
